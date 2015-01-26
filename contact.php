1a3fc0c27d5fef10b8d754686f2f8c663f1afb3d
ziyang <ziyang0621@gmail.com>
2015-01-26 15:00:20 -0800

    project update

--- contact.php
+++ contact.php
@@ -6,50 +6,49 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 	$message= trim($_POST["message"]);
 
 	if ($name == "" OR $email == "" OR $message == "") {
-		echo "YOu must specify a value for name, email address, and message.";
-		exit;
+		$error_message = "You must specify a value for name, email address, and message.";
 	}
 
-	foreach($_POST as $value) {
-		if (stripos($value, 'Contact-Type') !== FALSE) {
-			echo "There was a problem with the information you entered.";
-			exit;
+	if (!isset($error_message)) {
+		foreach($_POST as $value) {
+			if (stripos($value, 'Contact-Type') !== FALSE) {
+				$error_message = "There was a problem with the information you entered.";
+			}
 		}
 	}
 
-	if ($_POST["address"] != "") {
-		echo "Your form submission has an error.";
-		exit;
+	if (!isset($error_message) AND $_POST["address"] != "") {
+		$error_message = "Your form submission has an error.";
 	}
 
 	require_once("inc/phpmailer/class.phpmailer.php");
 	$mail = new PHPMailer();
 
-	if(!$mail->ValidateAddress($email)) {
-		echo "You must specify a valid email address.";
-		exit;
+	if(!isset($error_message) AND !$mail->ValidateAddress($email)) {
+		$error_message = "You must specify a valid email address.";
 	}
 
-	$email_body = "";
-	$email_body = $email_body . "Name: " . $name . "<br>";
-	$email_body = $email_body . "Email: " . $email . "<br>";
-	$email_body = $email_body . "Message: " . $message;
+	if (!isset($error_message)) {
+		$email_body = "";
+		$email_body = $email_body . "Name: " . $name . "<br>";
+		$email_body = $email_body . "Email: " . $email . "<br>";
+		$email_body = $email_body . "Message: " . $message;
 
-	$mail->SetFrom($email, $name);
-	$address = "order@shirts4mike.com";
-	$mail->AddAddress($address, "Shirts 4 Mike");
+		$mail->SetFrom($email, $name);
+		$address = "order@shirts4mike.com";
+		$mail->AddAddress($address, "Shirts 4 Mike");
 
-	$mail->Subject = "Shirts 4 Mike Contact Form Submission | " . $name;
+		$mail->Subject = "Shirts 4 Mike Contact Form Submission | " . $name;
 
-	$mail->MsgHTML($email_body);
+		$mail->MsgHTML($email_body);
 
-	if (!$mail->Send()) {
-		echo "There was a problem sending the email: " . $mail->ErrorInfo;
-		exit;
-	} 
-
-	header("Location: contact.php?status=thanks");
-	exit;
+		if ($mail->Send()) {
+			header("Location: contact.php?status=thanks");
+			exit;
+		} else {
+			$error_message =  "There was a problem sending the email: " . $mail->ErrorInfo;
+		}
+	}
 }
 
 ?><?php 
@@ -67,7 +66,13 @@ include('inc/header.php'); ?>
 			 	<p>Thanks for the email! I&rsquo;ll be in touch shortly.</p>
 			 <?php } else { ?>
 
-			 <p>I&rsquo;d love to hear from you! Complete the form to send me an email.</p>
+				 <?php 
+					 if (!isset($error_message)) {
+					 	echo '<p>I&rsquo;d love to hear from you! Complete the form to send me an email.</p>';
+					 } else {
+						echo '<p class="message">' . $error_message . '</p>';
+					}
+				?>
 
 				 <form method="post" action="contact.php">
 
@@ -77,7 +82,7 @@ include('inc/header.php'); ?>
 					 			<label for="name">Name</lable> 
 					 		</th>
 					 		<td>
-					 			<input type="text" name="name" id="name">
+					 			<input type="text" name="name" id="name" value="<?php if (isset($name)) { echo htmlspecialchars($name); } ?>">
 					 		</td>
 					 	</tr>
 					 	<tr>
@@ -85,7 +90,7 @@ include('inc/header.php'); ?>
 					 			<label for="email">Email</lable> 
 					 		</th>
 					 		<td>
-					 			<input type="text" name="email" id="email">
+					 			<input type="text" name="email" id="email" value="<?php if (isset($email)) { echo htmlspecialchars($email); } ?>">
 					 		</td>
 					 	</tr>
 					 	<tr>
@@ -93,7 +98,7 @@ include('inc/header.php'); ?>
 					 			<label for="message">Message</lable> 
 					 		</th>
 					 		<td>
-					 			<textarea name="message" id="message"></textarea>
+					 			<textarea name="message" id="message"><?php if (isset($message)) { echo htmlspecialchars($message); } ?></textarea>
 					 		</td>
 					 	</tr>
 					 	<tr style="display: none;">
