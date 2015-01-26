a8288f78aed75660fc652ecfb51a7b3a1247dff9
ziyang <ziyang0621@gmail.com>
2015-01-25 21:32:36 -0800

    first commit

---
+++ form.php
@@ -0,0 +1,21 @@
+<form target="paypal" action="https://www.paypal.com/cgi-bin/webscr" method="post">
+<input type="hidden" name="cmd" value="_s-xclick">
+<input type="hidden" name="hosted_button_id" value="HE7F8PRCX63KQ">
+<input type="hidden" name="item_name" value="Logo Shirt, Red">
+<table>
+<tr>
+	<th>
+		<input type="hidden" name="on0" value="Size">
+		<label for="os0">Size</label>
+	</th>
+	<td>
+<select name="os0" id="os0">
+	<option value="Small">Small </option>
+	<option value="Medium">Medium </option>
+	<option value="Large">Large </option>
+	<option value="X-Large">X-Large </option>
+</select> </td></tr>
+</table>
+<input type="submit" value="Add to Card" name="submit">
+</form>
+
