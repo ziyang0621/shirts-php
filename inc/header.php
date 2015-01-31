3c4913cf501deae5d97ffd440b37c050e1867f63
ziyang <ziyang0621@gmail.com>
2015-01-31 12:33:59 -0800

    project update

--- inc/header.php
+++ inc/header.php
@@ -16,6 +16,7 @@
 			<ul class="nav">
 				<li class="shirts <?php if ($section == "shirts") { echo "on"; } ?>"><a href="<?php echo BASE_URL; ?>shirts/">Shirts</a></li>
 				<li class="contact <?php if ($section =="contact") { echo "on"; } ?>"><a href="<?php echo BASE_URL; ?>contact/">Contact</a></li>
+				<li class="contact <?php if ($section =="search") { echo "on"; } ?>"><a href="<?php echo BASE_URL; ?>search/">Search</a></li>
 				<li class="cart"><a target="paypal" href="https://www.paypal.com/cgi-bin/webscr?cmd=_cart&amp;business=4ZN9BNQWJV8FL&amp;display=1">
 Shopping Cart</a></li>
 			</ul>
--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php 
 require_once("./inc/config.php");
+include(ROOT_PATH . "inc/products.php"); 
+$recent = get_products_recent();
 
 $pageTitle = "Unique T-shirts designed by a frog";
 include(ROOT_PATH . 'inc/header.php'); ?>
@@ -25,18 +27,12 @@ include(ROOT_PATH . 'inc/header.php'); ?>
 
 				<h2>Mike&rsquo;s Latest Shirts</h2>
 
-				<?php include(ROOT_PATH . "inc/products.php"); ?>
 				<ul class="products">
 					<?php 
-
-						$total_products = count($products);
-						$position = 0;
+					
 						$list_view_html = "";
-						foreach($products as $product_id => $product) {	
-							$position = $position + 1;
-							if ($total_products - $position < 4) {
-								$list_view_html = get_list_view_html($product_id, $product) . $list_view_html;
-							}
+						foreach($recent as $product) {	
+							$list_view_html = get_list_view_html($product) . $list_view_html;
 						}
 						echo $list_view_html;
 					?>					
